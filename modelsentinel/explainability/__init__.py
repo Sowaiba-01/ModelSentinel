cb165e91095402339715480b5f09a12217f2ca94
Sowaiba Arshad <sowaibaworkspace@gmail.com>
2026-07-28 16:00:00 +0500

    feat(explain): expose explainability public API

---
+++ modelsentinel/explainability/__init__.py
@@ -0,0 +1,5 @@
+from modelsentinel.explainability.effects import feature_effect
+from modelsentinel.explainability.gradcam import grad_cam
+from modelsentinel.explainability.importance import permutation_importance
+
+__all__ = ["permutation_importance", "feature_effect", "grad_cam"]
